cde2d57192d060d98cbd27694d5e43a0abad2c43
oleksandr.parfeniuk <o.parfeniuk@gmail.com>
2018-02-16 13:49:55 +0200

    Identity before customization

---
+++ Base2BaseWeb.UI/wwwroot/js/customLogOut.js
@@ -0,0 +1,20 @@
+﻿$('body').on('click', '.btn-logOut', function (e)
+{
+    e.preventDefault();
+    $.ajax({
+        url: $(this).attr('data-request-url').valueOf(),
+        type: 'POST',
+        dataType: "html",
+        success: function (data) {
+            window.location.href = 'http://localhost:53678/Home/Index';
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert('Error ' + ' - ' + textStatus);
+            //console.log(jqXHR);
+        }
+    });
+});
+//$('body').on('click', '.btn-logOut', function (e) {
+//    e.preventDefault();
+//    $.post($(this).attr('data-request-url').valueOf());
+//});
